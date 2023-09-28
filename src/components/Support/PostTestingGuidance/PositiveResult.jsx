8a0ad420e0d659f686c77486dbf39d119040abeb
Kha Tran <khatran961217@gmail.com>
2023-09-28 13:24:03 +1000

    Integrated find nearby clinics

--- src/components/Support/PostTestingGuidance/PositiveResult.jsx
+++ src/components/Support/PostTestingGuidance/PositiveResult.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { HiInformationCircle } from "react-icons/hi";
+import FindNearby from "./FindNearby";
 
 export default function PositiveResult() {
   const todoSteps = [
@@ -56,6 +57,8 @@ export default function PositiveResult() {
           ))}
         </div>
       </div>
+
+      <FindNearby />
     </div>
   );
 }
