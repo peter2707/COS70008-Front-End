f0b4f6071bba8370e76b09c3fea9726dc3682f23
Kha Tran <khatran961217@gmail.com>
2023-10-11 12:26:29 +1100

    Separated Topics from Content() into its own component

--- src/components/Learn/Content.jsx
+++ src/components/Learn/Content.jsx
@@ -1,9 +1,8 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import parse from "html-react-parser";
-import { TbMenuDeep } from "react-icons/tb";
-import { HiOutlineX } from "react-icons/hi";
 import TopicOverview from "./TopicOverview";
+import Topic from "./Topic";
 
 const topicsUrl = "http://localhost:3000/content/topics";
 const contentUrlBase = "http://localhost:3000/content/page/";
@@ -98,119 +97,15 @@ export default function Content() {
 
   return (
     <div className="relative top-24 z-10 learn-container flex flex-col lg:flex-row">
-      {/* Left panel desktop - topics navigation */}
-      <div className="topics-wrapper-desktop hidden lg:flex bg-white w-1/5 px-4 py-8">
-        {isLoading ? (
-          <p>Loading...</p>
-        ) : (
-          <div className="fixed">
-            <h6
-              className={`font-semibold text-lg lg:text-xl cursor-pointer p-0 mb-4 ${
-                showOverview
-                  ? "text-primary"
-                  : "hover:text-primary transition-colors duration-75"
-              }`}
-              onClick={handleOverviewClick}
-            >
-              Overview
-            </h6>
-            {/* Toggle Overview */}
-            {topics.map((topic) => (
-              <div key={topic._id}>
-                <h6 className="font-semibold text-lg lg:text-xl">
-                  {topic.name}
-                </h6>
-
-                <div className="sub-topics-container ml-4 mt-2">
-                  <ul>
-                    {topic.sub_topics.map((sub_topic) => (
-                      <li
-                        className={`cursor-pointer text-base lg:text-lg ${
-                          !showOverview
-                            ? "text-primary font-semibold"
-                            : "text-black hover:text-primary transition-colors duration-75"
-                        }`}
-                        key={sub_topic._id}
-                        onClick={() =>
-                          handleTopicSelection(sub_topic.content_id)
-                        }
-                      >
-                        {sub_topic.name}
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              </div>
-            ))}
-          </div>
-        )}
-      </div>
-
-      {/* Left panel mobile - topics navigation */}
-      <div className="topics-wrapper-mobile lg:hidden w-full">
-        <div className="fixed bg-white w-full left-0 px-4 py-2">
-          <button onClick={toggleSidebar}>
-            <TbMenuDeep className="text-3xl border rounded-md" />
-          </button>
-        </div>
-
-        {activateSidebar && (
-          <div className="fixed flex flex-row w-full h-full">
-            <div className="bg-white w-3/5 min-w-max px-4 pt-8">
-              <button
-                className="w-full flex justify-end"
-                onClick={toggleSidebar}
-              >
-                <HiOutlineX className="text-3xl border rounded-md" />
-              </button>
-              {isLoading ? (
-                <p>Loading...</p>
-              ) : (
-                <div>
-                  <h6
-                    className={`font-semibold text-lg lg:text-xl cursor-pointer p-0 mb-4 ${
-                      showOverview
-                        ? "text-primary"
-                        : "hover:text-primary transition-colors duration-75"
-                    }`}
-                    onClick={handleOverviewClick}
-                  >
-                    Overview
-                  </h6>
-                  <div className="flex items-start justify-start">
-                    {topics.map((topic) => (
-                      <div key={topic._id} className="mb-4">
-                        <h6 className="font-semibold text-lg lg:text-xl">
-                          {topic.name}
-                        </h6>
-                        <ul>
-                          {topic.sub_topics.map((sub_topic) => (
-                            <li
-                              className={`cursor-pointer text-base lg:text-lg ml-4 my-2 ${
-                                !showOverview
-                                  ? "text-primary font-semibold"
-                                  : "text-black hover:text-primary transition-colors duration-75"
-                              }`}
-                              key={sub_topic._id}
-                              onClick={() =>
-                                handleTopicSelection(sub_topic.content_id)
-                              }
-                            >
-                              {sub_topic.name}
-                            </li>
-                          ))}
-                        </ul>
-                      </div>
-                    ))}
-                  </div>
-                </div>
-              )}
-            </div>
-
-            <div className="bg-gray-500/20 backdrop-blur-sm w-full"></div>
-          </div>
-        )}
-      </div>
+      <Topic
+        topics={topics}
+        isLoading={isLoading}
+        showOverview={showOverview}
+        handleOverviewClick={handleOverviewClick}
+        handleTopicSelection={handleTopicSelection}
+        toggleSidebar={toggleSidebar}
+        activateSidebar={activateSidebar}
+      />
 
       {/* Display content on the right */}
       <div className="content-wrapper bg-gray-50 w-full">
---
+++ src/components/Learn/Topic.jsx
@@ -0,0 +1,156 @@
+import React from "react";
+//import { HiBookmark } from "react-icons/hi";
+import { TbMenuDeep } from "react-icons/tb";
+import { HiOutlineX } from "react-icons/hi";
+//import { Form } from "react-router-dom";
+
+export default function Topic({
+  topics,
+  isLoading,
+  showOverview,
+  handleOverviewClick,
+  handleTopicSelection,
+  toggleSidebar,
+  activateSidebar,
+}) {
+  return (
+    <>
+      {/* Left panel desktop - topics navigation */}
+      <div className="topics-wrapper-desktop hidden lg:flex bg-white w-1/5 px-4 py-8">
+        {isLoading ? (
+          <p>Loading...</p>
+        ) : (
+          <div className="fixed">
+            <h6
+              className={`font-semibold text-lg lg:text-xl cursor-pointer p-0 mb-4 ${
+                showOverview
+                  ? "text-primary"
+                  : "hover:text-primary transition-colors duration-75"
+              }`}
+              onClick={handleOverviewClick}
+            >
+              Overview
+            </h6>
+            {/* Toggle Overview */}
+            {topics.map((topic) => (
+              <div key={topic._id}>
+                <h6 className="font-semibold text-lg lg:text-xl">
+                  {topic.name}
+                </h6>
+
+                <div className="sub-topics-container ml-4 mt-2">
+                  <ul>
+                    {topic.sub_topics.map((sub_topic) => (
+                      <li
+                        className={`cursor-pointer text-base lg:text-lg ${
+                          !showOverview
+                            ? "text-primary font-semibold"
+                            : "text-black hover:text-primary transition-colors duration-75"
+                        }`}
+                        key={sub_topic._id}
+                        onClick={() =>
+                          handleTopicSelection(sub_topic.content_id)
+                        }
+                      >
+                        {sub_topic.name}
+                        {/*<Bookmark />*/}
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+
+      {/* Left panel mobile - topics navigation */}
+      <div className="topics-wrapper-mobile lg:hidden w-full">
+        <div className="fixed bg-white w-full left-0 px-4 py-2">
+          <button onClick={toggleSidebar}>
+            <TbMenuDeep className="text-3xl border rounded-md" />
+          </button>
+        </div>
+
+        {activateSidebar && (
+          <div className="fixed flex flex-row w-full h-full">
+            <div className="bg-white w-3/5 min-w-max px-4 pt-8">
+              <button
+                className="w-full flex justify-end"
+                onClick={toggleSidebar}
+              >
+                <HiOutlineX className="text-3xl border rounded-md" />
+              </button>
+              {isLoading ? (
+                <p>Loading...</p>
+              ) : (
+                <div>
+                  <h6
+                    className={`font-semibold text-lg lg:text-xl cursor-pointer p-0 mb-4 ${
+                      showOverview
+                        ? "text-primary"
+                        : "hover:text-primary transition-colors duration-75"
+                    }`}
+                    onClick={handleOverviewClick}
+                  >
+                    Overview
+                  </h6>
+                  <div className="flex items-start justify-start">
+                    {topics.map((topic) => (
+                      <div key={topic._id} className="mb-4">
+                        <h6 className="font-semibold text-lg lg:text-xl">
+                          {topic.name}
+                        </h6>
+                        <ul>
+                          {topic.sub_topics.map((sub_topic) => (
+                            <li
+                              className={`cursor-pointer text-base lg:text-lg ml-4 my-2 ${
+                                !showOverview
+                                  ? "text-primary font-semibold"
+                                  : "text-black hover:text-primary transition-colors duration-75"
+                              }`}
+                              key={sub_topic._id}
+                              onClick={() =>
+                                handleTopicSelection(sub_topic.content_id)
+                              }
+                            >
+                              {sub_topic.name}
+                            </li>
+                          ))}
+                        </ul>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              )}
+            </div>
+
+            <div className="bg-gray-500/20 backdrop-blur-sm w-full"></div>
+          </div>
+        )}
+      </div>
+    </>
+  );
+}
+
+/* TODO: To be used later for bookmark feature
+
+function Bookmark({ topic }) {
+  let bookmark = topic.subtopic.bookmark;
+  return (
+    <Form method="post">
+      <button
+        name="bookmark"
+        value={bookmark ? "false" : "true"}
+        aria-label={bookmark ? "Remove from bookmarks" : "Add to bookmarks"}
+      >
+        {bookmark ? (
+          <HiBookmark className="text-primary" />
+        ) : (
+          <HiBookmark className="text-gray-200" />
+        )}
+      </button>
+    </Form>
+  );
+}
+*/
