e3e6edf0161bf5e80784c5a5c3cc65101ed78e54
Peter2707 <prummonkolsophearith@gmail.com>
2023-10-14 14:32:19 +1100

    added content management page and integration, added email list page and integration, fixed user page css

--- src/components/AdminDashboard/ContentManagement.js
+++ src/components/AdminDashboard/ContentManagement.js
@@ -1,20 +1,144 @@
+import React, { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import "./ContentManagement.css";
+
 export function ContentManagement() {
+    const [contentType, setContentType] = useState("FAQ");
+    const [question, setQuestion] = useState("");
+    const [answer, setAnswer] = useState("");
+    const [articleContent, setArticleContent] = useState("");
+    const [authorName, setAuthorName] = useState("");
+    const [date, setDate] = useState("");
+    const [isDateInputFocused, setDateInputFocused] = useState(false);
+
+    const handleFAQSubmit = async (e) => {
+        e.preventDefault();
+
+        // Validation checks
+        if (!question.trim() || !answer.trim()) {
+            window.alert(
+                "Please provide both question and answer for the FAQ."
+            );
+            return;
+        }
+
+        const response = await fetch("/api/save-faq", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ question, answer }),
+        });
+
+        if (response.ok) {
+            setQuestion("");
+            setAnswer("");
+            window.alert("FAQ successfully saved!");
+        }
+    };
+
+    const handleArticleSubmit = async (e) => {
+        e.preventDefault();
+
+        // Validation checks
+        if (!authorName.trim() || !date || !articleContent.trim()) {
+            window.alert(
+                "Please provide the author name, date, and article content."
+            );
+            return;
+        }
+
+        const response = await fetch("/api/save-article", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ authorName, date, content: articleContent }),
+        });
+
+        if (response.ok) {
+            setAuthorName("");
+            setDate("");
+            setArticleContent("");
+            window.alert("Article successfully saved!");
+        }
+    };
+
     return (
         <div>
             <h1>Content Management</h1>
             <hr />
-            <h1>Add an FAQ</h1>
-            <input type="text" placeholder="Enter FAQ title" />
-            <div 
-                contentEditable={true} 
-                style={{
-                    border: '1px solid #ccc',
-                    padding: '10px',
-                    minHeight: '100px',
-                    marginTop: '10px',
-                }}
-                placeholder="Enter FAQ description"
-            ></div>
+
+            <label className="mt-5">
+                Choose Content Type:
+                <select
+                    value={contentType}
+                    onChange={(e) => setContentType(e.target.value)}
+                >
+                    <option value="FAQ">FAQ</option>
+                    <option value="Article">Article</option>
+                </select>
+            </label>
+
+            {contentType === "FAQ" && (
+                <form onSubmit={handleFAQSubmit}>
+                    <h5>Add a FAQ</h5>
+                    <input
+                        type="text"
+                        placeholder="Enter FAQ title"
+                        value={question}
+                        onChange={(e) => setQuestion(e.target.value)}
+                    />
+                    <ReactQuill
+                        className="react-quill-faq"
+                        value={answer}
+                        onChange={setAnswer}
+                        placeholder="Enter FAQ description"
+                        theme="snow"
+                    />
+                    <button className="submit-btn" type="submit">
+                        Submit FAQ
+                    </button>
+                </form>
+            )}
+
+            {contentType === "Article" && (
+                <form onSubmit={handleArticleSubmit}>
+                    <h5>Add an Article</h5>
+                    <input
+                        type="text"
+                        placeholder="Author's Name"
+                        value={authorName}
+                        onChange={(e) => setAuthorName(e.target.value)}
+                    />
+                    {!isDateInputFocused ? (
+                        <input
+                            type="text"
+                            placeholder="Publication Date"
+                            onFocus={() => setDateInputFocused(true)}
+                        />
+                    ) : (
+                        <input
+                            type="date"
+                            value={date}
+                            onBlur={() => setDateInputFocused(false)}
+                            onChange={(e) => setDate(e.target.value)}
+                            required
+                        />
+                    )}
+                    <ReactQuill
+                        className="react-quill-article"
+                        value={articleContent}
+                        onChange={setArticleContent}
+                        placeholder="Enter Article Content"
+                        theme="snow"
+                    />
+                    <button className="submit-btn" type="submit">
+                        Submit Article
+                    </button>
+                </form>
+            )}
         </div>
     );
 }
--- src/components/AdminDashboard/Email.js
+++ src/components/AdminDashboard/Email.js
@@ -1,3 +1,132 @@
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+import { FaPen } from "react-icons/fa";
+import "./User.css";
+import "./Email.css";
+
 export function Email() {
-    return <div>Email Reminder</div>;
+    const [users, setUsers] = useState([]);
+    const [displayedUsers, setDisplayedUsers] = useState([]);
+    const [showEditPopup, setShowEditPopup] = useState(false);
+    const [selectedUser, setSelectedUser] = useState(null);
+    const [searchTerm, setSearchTerm] = useState("");
+
+    useEffect(() => {
+        const fetchEmailReminderUsers = async () => {
+            try {
+                const token = localStorage.getItem("token");
+                const response = await axios.get('http://localhost:3000/admin/email-reminder-users', {
+                    headers: {
+                        'Authorization': `Bearer ${token}`
+                    }
+                });
+
+                setUsers(response.data);
+                setDisplayedUsers(response.data);
+            } catch (error) {
+                console.error("Error fetching the users", error);
+            }
+        }
+
+        fetchEmailReminderUsers();
+    }, []);
+
+    const handleEdit = (user) => {
+        setSelectedUser(user);
+        setShowEditPopup(true);
+    };
+
+    const handleClosePopup = () => {
+        setSelectedUser(null);
+        setShowEditPopup(false);
+    };
+
+    const handleSearch = () => {
+        const filteredUsers = users.filter(user =>
+            user.email.toLowerCase().includes(searchTerm.toLowerCase()) ||
+            user.id.toString().includes(searchTerm)
+        );
+        setDisplayedUsers(filteredUsers);
+    };
+
+    return (
+        <div className="email-container">
+            <h1>Email Reminders</h1>
+            <hr />
+            <div className="search-bar">
+                <input
+                    type="text"
+                    placeholder="Search by email or ID..."
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                />
+                <button onClick={handleSearch}>Search</button>
+            </div>
+            <table className="users-table">
+                <thead>
+                    <tr>
+                        <th>User ID</th>
+                        <th>Email Address</th>
+                        <th>Email Frequency</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {displayedUsers.map((user) => (
+                        <tr key={user.id}>
+                            <td>{user.id}</td>
+                            <td>{user.email}</td>
+                            <td>{user.emailFrequency}</td>
+                            <td>
+                                <button
+                                    className="edit-btn"
+                                    onClick={() => handleEdit(user)}
+                                >
+                                    <FaPen className="text-xl" />
+                                </button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+            {showEditPopup && (
+                <EditEmailFrequencyPopup user={selectedUser} onClose={handleClosePopup} />
+            )}
+        </div>
+    );
+}
+
+function EditEmailFrequencyPopup({ user, onClose }) {
+    const [frequency, setFrequency] = useState(user.emailFrequency);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Logic to update the user's email frequency in the backend
+        // axios.put('UPDATE ENDPOINT HERE', { userId: user.id, emailFrequency: frequency });
+        onClose();
+    };
+
+    return (
+        <div className="popup">
+            <div className="popup-content">
+                <h5>Edit Email Frequency</h5>
+                <hr />
+                <br />
+                <form onSubmit={handleSubmit}>
+                    <label>
+                        Email Frequency:
+                        <select className="email-select" value={frequency} onChange={(e) => setFrequency(e.target.value)}>
+                            <option value="weekly">Weekly</option>
+                            <option value="monthly">Monthly</option>
+                            <option value="yearly">Yearly</option>
+                        </select>
+                    </label>
+                    <div className="popup-actions">
+                        <button type="submit" className="submit-btn">Update</button>
+                        <button type="button" className="cancel-btn" onClick={onClose}>Cancel</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
 }
