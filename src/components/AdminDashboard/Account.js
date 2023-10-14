d9f8f0976aee19881bdb0eef732d1bacef60b5f0
Peter2707 <prummonkolsophearith@gmail.com>
2023-10-14 15:07:18 +1100

    added admin account setting form, password change, and logout feature. ready for integration

--- src/components/AdminDashboard/Account.js
+++ src/components/AdminDashboard/Account.js
@@ -1,3 +1,189 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./Account.css";
+
 export function Account() {
-    return <div>Account Setting</div>;
+    const [adminData, setAdminData] = useState({
+        id: "",
+        firstName: "",
+        lastName: "",
+        email: "",
+        role: "",
+    });
+    const [oldPassword, setOldPassword] = useState("");
+    const [newPassword, setNewPassword] = useState("");
+    const [confirmNewPassword, setConfirmNewPassword] = useState("");
+
+    useEffect(() => {
+        const fetchAdminDetails = async () => {
+            const token = localStorage.getItem("token");
+            try {
+                const response = await axios.get(
+                    "http://localhost:3000/admin/account-details",
+                    {
+                        headers: {
+                            Authorization: `Bearer ${token}`,
+                        },
+                    }
+                );
+
+                setAdminData(response.data);
+            } catch (error) {
+                console.error("Error fetching admin account details:", error);
+            }
+        };
+
+        fetchAdminDetails();
+    }, []);
+
+    const handleAccountUpdate = async (e) => {
+        e.preventDefault();
+
+        const token = localStorage.getItem("token");
+        try {
+            await axios.put(
+                "http://localhost:3000/admin/update-account",
+                adminData,
+                {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                }
+            );
+            alert("Account details updated successfully!");
+        } catch (error) {
+            console.error("Error updating account details:", error);
+            alert("Failed to update account details.");
+        }
+    };
+
+    const handlePasswordChange = async (e) => {
+        e.preventDefault();
+
+        if (newPassword !== confirmNewPassword) {
+            alert("New passwords do not match.");
+            return;
+        }
+
+        const token = localStorage.getItem("token");
+        try {
+            await axios.put(
+                "http://localhost:3000/admin/change-password",
+                {
+                    oldPassword,
+                    newPassword,
+                },
+                {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                }
+            );
+            alert("Password changed successfully!");
+        } catch (error) {
+            console.error("Error changing password:", error);
+            alert("Failed to change password.");
+        }
+    };
+
+    return (
+        <div>
+            <div className="account-setting-section">
+                <h2>Account Settings</h2>
+                <br />
+                <hr />
+                <br />
+                <form onSubmit={handleAccountUpdate}>
+                    <label>
+                        Admin ID:
+                        <input type="text" value={adminData.id} readOnly />
+                    </label>
+                    <label>
+                        First Name:
+                        <input
+                            type="text"
+                            value={adminData.firstName}
+                            onChange={(e) =>
+                                setAdminData({
+                                    ...adminData,
+                                    firstName: e.target.value,
+                                })
+                            }
+                        />
+                    </label>
+                    <label>
+                        Last Name:
+                        <input
+                            type="text"
+                            value={adminData.lastName}
+                            onChange={(e) =>
+                                setAdminData({
+                                    ...adminData,
+                                    lastName: e.target.value,
+                                })
+                            }
+                        />
+                    </label>
+                    <label>
+                        Email:
+                        <input
+                            type="email"
+                            value={adminData.email}
+                            onChange={(e) =>
+                                setAdminData({
+                                    ...adminData,
+                                    email: e.target.value,
+                                })
+                            }
+                        />
+                    </label>
+                    <label>
+                        Role:
+                        <input type="text" value={adminData.role} readOnly />
+                    </label>
+
+                    <button className="update-account-btn" type="submit">
+                        Update Account
+                    </button>
+                </form>
+            </div>
+
+            <div className="change-password-section">
+                <h3>Change Password</h3>
+                <br />
+                <hr />
+                <br />
+                <form onSubmit={handlePasswordChange}>
+                    <label>
+                        Current Password:
+                        <input
+                            type="password"
+                            value={oldPassword}
+                            onChange={(e) => setOldPassword(e.target.value)}
+                        />
+                    </label>
+                    <label>
+                        New Password:
+                        <input
+                            type="password"
+                            value={newPassword}
+                            onChange={(e) => setNewPassword(e.target.value)}
+                        />
+                    </label>
+                    <label>
+                        Confirm New Password:
+                        <input
+                            type="password"
+                            value={confirmNewPassword}
+                            onChange={(e) =>
+                                setConfirmNewPassword(e.target.value)
+                            }
+                        />
+                    </label>
+
+                    <button className="change-password-btn" type="submit">Change Password</button>
+                </form>
+            </div>
+        </div>
+    );
 }
