1b485aee568d098dcbd8578770f9b0a299c19d43
Peter2707 <prummonkolsophearith@gmail.com>
2023-10-14 23:05:38 +1100

    working on userdashboard integration and some css fixes

--- src/components/UserDashboard/UserDashboard.js
+++ src/components/UserDashboard/UserDashboard.js
@@ -1,37 +1,64 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { FaLink, FaPen } from "react-icons/fa6";
 import "./UserDashboard.css";
 
 const UserDashboard = () => {
+    const [userData, setUserData] = useState({});
+    const [articles, setArticles] = useState([]);
+
+    useEffect(() => {
+        // Fetch user details
+        axios
+            .get("/api/user-details")
+            .then((response) => {
+                setUserData(response.data);
+            })
+            .catch((error) => {
+                console.error("Error fetching user details:", error);
+            });
+
+        // Fetch articles (assuming the backend returns an array of articles)
+        axios
+            .get("/api/user-articles")
+            .then((response) => {
+                setArticles(response.data);
+            })
+            .catch((error) => {
+                console.error("Error fetching articles:", error);
+            });
+    }, []);
     return (
-        <div className="dashboard-container w-full lg:max-w-screen-2xl mx-auto">
+        <div className="dashboard-container w-full lg:max-w-screen-2xl mx-auto mt-24">
             <div className="main-content">
                 <div className="header-content">
-                    <h1>Good Afternoon, Jason</h1>
-                    <p>Wednesday, 30th August</p>
+                    <h1>Good Afternoon, {userData.name}</h1>
+                    <p>{userData.date}</p>
                 </div>
                 <div className="record-section">
                     <h1 className="text-primary">Next Periodic Test</h1>
-                    <h3>12th December</h3>
+                    <h3>{userData.nextTestDate}</h3>
                     <br />
                     <p>Last Recorded Test</p>
-                    <p>12th June | Negative</p>
+                    <p>
+                        {userData.lastTestDate} | {userData.lastTestResult}
+                    </p>
                     <br />
-                    <button className="primary-btn">Add Record</button>
-                    <button className="outline-btn">Download Record</button>
+                    <button className="add-record-btn">Add Record</button>
+                    <button className="download-record-btn">
+                        Download Record
+                    </button>
                 </div>
                 <div className="shortcut-section">
-                    <h2>Title</h2>
-                    <h4>Subtitle</h4>
                     <button className="expand-all-btn">Expand All</button>
+                    <h2>Your Shortcuts</h2>
+                    <p>Topics you bookmarked from Learn will display here</p>
+
                     <div className="articles">
-                        {[...Array(4)].map((_, index) => (
+                        {articles.map((article, index) => (
                             <div key={index} className="article-card">
-                                <h5>Article Title {index + 1}</h5>
-                                <p>
-                                    The distinction between HIV and AIDS and how
-                                    HIV weakens the immune system and can
-                                    progress to AIDS.
-                                </p>
+                                <h5>{article.title}</h5>
+                                <p>{article.content}</p>
                                 <button className="learn-more-btn">
                                     Learn More
                                 </button>
@@ -44,11 +71,24 @@ const UserDashboard = () => {
                 <div className="community-section">
                     <h6>Community Participation</h6>
                     <p>Answer HIV questionaires</p>
-                    <button>Start</button>
+                    <button className="answer-community-qa-btn">Start</button>
                 </div>
                 <div className="email-notification-section">
+                <button className="set-email-btn"><FaPen /></button>
                     <h6>Email Notification</h6>
                     <p>Every 3 months</p>
+                </div>
+                <br />
+                <hr />
+                <br />
+                <div className="get-testkit-section">
+                    <h6>Get HIV self-testing kit</h6>
+                    <a
+                        href="https://atomohivtest.com/home.php"
+                        className="purchase-link"
+                    >
+                        Purchase test kit <FaLink />
+                    </a>
                 </div>
             </div>
         </div>
