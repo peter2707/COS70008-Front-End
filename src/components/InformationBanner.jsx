cc6201dd4a37745fc4d331c0d92ddfeb0cb5f00f
Khaxtran <68594116+Khaxtran@users.noreply.github.com>
2023-10-18 21:55:37 +1100

    removed token for learn (#39)

--- src/components/InformationBanner.jsx
+++ src/components/InformationBanner.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { HiInformationCircle } from "react-icons/hi";
+import { Link } from "react-router-dom";
 
 export default function InformationBanner({ textBold, text, linkName, href }) {
   return (
@@ -8,7 +9,7 @@ export default function InformationBanner({ textBold, text, linkName, href }) {
       <p>
         <span className="font-semibold">{textBold}</span>
         {text}
-        <a className="link" href={href} title={linkName}>{linkName}</a>
+        <Link className="link" to={href} title={linkName}>{linkName}</Link>
       </p>
     </div>
   );
